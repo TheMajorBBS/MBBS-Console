63091a262b3c9b2890d4f13f2297c1cd7207883b
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-08-02 12:43:41 +0000

    adding print statements to debug windows version

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -83,7 +83,7 @@ class FFAppState extends ChangeNotifier {
 
   late SharedPreferences prefs;
 
-  String _version = '0.2.1';
+  String _version = '0.2.2';
   String get version => _version;
   set version(String value) {
     _version = value;
--- lib/custom_code/widgets/web_socket_conn.dart
+++ lib/custom_code/widgets/web_socket_conn.dart
@@ -3,6 +3,7 @@ import '/backend/schema/structs/index.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'index.dart'; // Imports other custom widgets
+import '/custom_code/actions/index.dart'; // Imports custom actions
 import '/flutter_flow/custom_functions.dart'; // Imports custom functions
 import 'package:flutter/material.dart';
 // Begin custom widget code
@@ -58,7 +59,7 @@ class _WebSocketConnState extends State<WebSocketConn> {
     await _channel!.ready;
 
     _channel!.stream.listen((event) {
-      print(event.toString());
+      //print(event.toString());
       myMessage = '${event}';
       print(myMessage);
       FFAppState().wsMessage = myMessage;
