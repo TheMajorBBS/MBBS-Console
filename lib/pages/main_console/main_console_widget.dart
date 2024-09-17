63bd723e3beab52bf5c2f79812bd77028afc28d0
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-09-17 16:44:11 +0000

    grid adjustments and logging on the auth component

--- lib/pages/main_console/main_console_widget.dart
+++ lib/pages/main_console/main_console_widget.dart
@@ -1,4 +1,4 @@
-import '/components/channel_grid_widget.dart';
+import '/components/channel_grid/channel_grid_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/instant_timer.dart';
