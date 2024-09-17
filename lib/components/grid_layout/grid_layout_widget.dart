27c5d6024dada9f58e468b0d512eaa9b16463c2a
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-09-17 18:56:31 +0000

    try child aspect ratio for channel grid

--- lib/components/grid_layout/grid_layout_widget.dart
+++ lib/components/grid_layout/grid_layout_widget.dart
@@ -54,7 +54,7 @@ class _GridLayoutWidgetState extends State<GridLayoutWidget> {
               crossAxisCount: 16,
               crossAxisSpacing: 0.0,
               mainAxisSpacing: 0.0,
-              childAspectRatio: 1.0,
+              childAspectRatio: 0.45,
             ),
             scrollDirection: Axis.horizontal,
             itemCount: chnnelGridList.length,
